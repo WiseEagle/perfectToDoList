40dbd8deb412d5649387916d29fd60b98f4bdc99
Volodymyr Khomenko <wiseeaglesc@gmail.com>
2024-01-24 00:00:21 +0200

    First step to make my own perfect ToDoList

---
+++ scripts/first.js
@@ -0,0 +1,36 @@
+const numberOfToDos = prompt("How many to do's do you have?");
+
+const toDoList = {
+    numberOfToDos: numberOfToDos,
+    toDos: [
+        {
+            title: 'Get out of bed',
+            status: 'done'
+        },
+    ],
+    addToDo: function (toDo) {
+        let toDoObject = {
+            title: toDo,
+            status: 'open'
+        };
+        this.toDos.push(toDoObject);
+    },
+    showToDos: function () {
+        console.log(`You have ${this.numberOfToDos} to do's`);
+        this.toDos.forEach((toDo, index) => {
+            console.log(`${index + 1}. ${toDo['title']} - ${toDo['status']}`);
+        });
+    },
+    changeStatus: function (index, status) {
+        this.toDos[index]['status'] = status;
+    }
+
+
+};
+
+const toDo = prompt("What do you need to do?");
+
+toDoList.addToDo(toDo);
+toDoList.showToDos();
+toDoList.changeStatus(1, 'done');
+toDoList.showToDos();
