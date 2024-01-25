77bf4e2ab16db06da5854a6c6585e93185759683
Volodymyr Khomenko <wiseeaglesc@gmail.com>
2024-01-25 23:52:58 +0200

    Add a python courses files in toDo

---
+++ server/server.py
@@ -0,0 +1,12 @@
+toDos = {
+    "1": {
+        "title": "Start Python Course",
+        "description": "Create a simple Python course on Udemy",
+        "status": "done"
+    },
+    "2": {
+        "title": "Wake up early",
+        "description": "Wake up at 7:00 AM and make a coffee",
+        "status": "open"
+    }
+}
